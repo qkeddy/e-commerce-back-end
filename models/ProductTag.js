2ebad71dab8eec2e225e3d2b367ed0d584a72537
Quin Eddy <qkeddy@gmail.com>
2022-04-23 16:07:45 -0400

    All data seeded

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,20 +1,44 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 class ProductTag extends Model {}
 
 ProductTag.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product_tag',
-  }
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+
+        // Store a reference of the `id` of the `product`
+        product_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "product",
+                key: "id",
+                unique: false,
+            },
+        },
+        // Store a reference of the `id` of the `tag`
+        tag_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "tag",
+                key: "id",
+                unique: false,
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "product_tag",
+    }
 );
 
 module.exports = ProductTag;
