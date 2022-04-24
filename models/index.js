bc2591be87e85bfe8b21c84ba1d88cbfc6a5237a
Quin Eddy <qkeddy@gmail.com>
2022-04-24 16:16:52 -0400

    Completed the routes for Categories. Added foreign key reference for the category to product one to many relationship.

--- models/index.js
+++ models/index.js
@@ -5,11 +5,10 @@ const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
 
 // Categories have many Products
-Category.hasMany(Product);
+Category.hasMany(Product, { foreignKey: "category_id" });
 
 // Products belongsTo Category
-Product.belongsTo(Category);
-
+Product.belongsTo(Category, { foreignKey: "category_id" });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
